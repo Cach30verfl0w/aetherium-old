c5cadc8c09c8b2a0b98e2287a17105b5e3a69fec
Cach30verfl0w <cach30verfl0w@gmail.com>
2024-01-30 18:13:20 +0100

    Initial commit

--- test/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-auto main(int num_args, char** args) -> int {
-	testing::InitGoogleTest(&num_args, args);
-	return RUN_ALL_TESTS();
-}
--- test/my_test.cpp
+++ test/my_test.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include "aetherium/aetherium.hpp"
 
 TEST(example, TestMyObject) {
+    aetherium::Application application {};
+
 	std::cout << "Hello unit test!\n";
 }
